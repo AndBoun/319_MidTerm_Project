8a8e2dd16e54c7284ea0c0de1d805829898196a8
Kai Quach <kaiquach04@gmail.com>
2024-10-17 00:48:48 -0500

    some javascript features
    sfsadaswd

---
+++ js/login.js
@@ -0,0 +1,36 @@
+function fetchUser(){
+    document.getElementById('login').innerHTML = 'Fetching user...';
+    return new Promise((resolve, reject) => {
+        fetch("./js/password.json")  // Ensure the path is correct
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => resolve(data))
+        .catch((error) => {
+            console.log(error);
+            document.getElementById('login').innerHTML = 'Error fetching user!';
+            reject(error);
+        });
+    });
+}
+
+function login(user, passwordInput){
+    if (user.password === passwordInput){
+        document.getElementById('login').innerHTML = 'Login successful!';
+        window.location.href = 'homepage.html';  // Redirect to new HTML page
+    } else {
+        document.getElementById('login').innerHTML = 'Login failed!';
+    }
+    console.log(passwordInput);
+}
+
+document.getElementById('loginform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const passwordInput = document.getElementById('passwordInput').value;
+    fetchUser().then(user => {
+        login(user, passwordInput);
+    });
+});
